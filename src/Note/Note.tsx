a40f05a440abc5778efef83871da72a0a1465e18
Lucif3r-in <ashutosh123rath@gmail.com>
2023-06-29 21:34:21 +0530

    fix: add share functionality

--- src/Note/Note.tsx
+++ src/Note/Note.tsx
@@ -1,7 +1,15 @@
+import React, { useState } from "react";
 import { Badge, Button, Col, Row, Stack } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import { useNote } from "../NoteLayout/Layout";
 import ReactMarkdown from "react-markdown";
+
+import { FacebookIcon, TwitterIcon, WhatsappIcon } from "react-share";
+import {
+  FacebookShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+} from "react-share";
 
 type NoteProps = {
   onDelete: (id: string) => void;
@@ -10,6 +18,13 @@ type NoteProps = {
 export function Note({ onDelete }: NoteProps) {
   const note = useNote();
   const navigate = useNavigate();
+
+  const [shareLink, setShareLink] = useState("");
+
+  const shareNote = () => {
+    const link = `${window.location.origin}/${note.id}`;
+    setShareLink(link);
+  };
 
   return (
     <>
@@ -40,13 +55,68 @@ export function Note({ onDelete }: NoteProps) {
             >
               Delete
             </Button>
+            <Button variant="outline-secondary" onClick={shareNote}>
+              Share
+            </Button>
             <Link to="/">
               <Button variant="outline-secondary">Back</Button>
             </Link>
           </Stack>
         </Col>
       </Row>
-      <ReactMarkdown>{note.markdown}</ReactMarkdown>
+      <div
+        style={{
+          border: "6px solid",
+          borderRadius: "12px",
+          backgroundColor: "white",
+          padding: "20px",
+          fontSize: "18px",
+          height: "500px",
+          overflowY: "scroll",
+        }}
+      >
+        <ReactMarkdown>{note.markdown}</ReactMarkdown>
+      </div>
+      {shareLink && (
+        <Row className="mt-4">
+          <Col>
+            <div className="d-flex justify-center gap-4 mb-2">
+              {" "}
+              {/* Add a class and specify margin-bottom */}
+              <div className="mr-2">
+                {" "}
+                {/* Add a class and specify margin-right */}
+                <FacebookShareButton url={shareLink}>
+                  <Button variant="outline-primary">
+                    <FacebookIcon size={24} round />
+                    Share on Facebook
+                  </Button>
+                </FacebookShareButton>
+              </div>
+              <div className="mr-2">
+                {" "}
+                {/* Add a class and specify margin-right */}
+                <TwitterShareButton url={shareLink}>
+                  <Button variant="outline-primary">
+                    <TwitterIcon size={24} round />
+                    Share on Twitter
+                  </Button>
+                </TwitterShareButton>
+              </div>
+              <div>
+                {" "}
+                {/* No need for margin */}
+                <WhatsappShareButton url={shareLink}>
+                  <Button variant="outline-primary">
+                    <WhatsappIcon size={24} round />
+                    Share on WhatsApp
+                  </Button>
+                </WhatsappShareButton>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      )}
     </>
   );
 }
